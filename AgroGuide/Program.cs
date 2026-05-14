d0c0bdcad07058c487f115a85bd01aa5696e5cfb
Sushanto <sushantovharati.sv@gmail.com>
2026-05-14 11:34:44 +0600

    Configure dependency injection in Program.cs

--- AgroGuide/Program.cs
+++ AgroGuide/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<FertilizerRepo>();
