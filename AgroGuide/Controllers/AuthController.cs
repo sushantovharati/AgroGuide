4b9a26b7490b83c4bf2a7b9a9462fe9a3fe1589b
Sushanto <sushantovharati.sv@gmail.com>
2026-05-14 09:27:07 +0600

    Add login and farmer registration UI

---
+++ AgroGuide/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AgroGuide.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
